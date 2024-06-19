accf295d04c43eb82f35b493e743ccab53352524
George Maia <georgemaiaf@gmail.com>
2024-06-18 23:21:58 -0300

    refactor(Backend): :recycle: Role

--- src/PontoSavi.Domain/Entities/Role.cs
+++ src/PontoSavi.Domain/Entities/Role.cs
@@ -1,19 +1,37 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
 
 namespace PontoSavi.Domain.Entities;
 
 public class Role : IdentityRole<int>
 {
-    public string PublicId { get; set; } = null!;
+    override public string Name { get; set; } = null!;
 
-    public Role()
-    {
-        PublicId = Ulid.NewUlid().ToString();
-    }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+
+    // [JsonIgnore]
+    public int CompanyId { get; set; }
+    [JsonIgnore]
+    public Company? Company { get; set; } = null;
+
+    public Role() { }
 
     public Role(string roleName)
     {
-        PublicId = Ulid.NewUlid().ToString();
         Name = roleName;
     }
+
+    public Role(Role role)
+    {
+        Id = role.Id;
+        Name = role.Name;
+        CompanyId = role.CompanyId;
+        Company = role.Company;
+    }
+
+    [JsonIgnore]
+    override public string? NormalizedName { get; set; }
+    [JsonIgnore]
+    override public string ConcurrencyStamp { get; set; } = Guid.NewGuid().ToString();
 }
