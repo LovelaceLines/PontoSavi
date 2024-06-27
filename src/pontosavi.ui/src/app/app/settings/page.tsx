279a9fc193ea1abd4c955f83d18676982cdc212a
George Maia <georgemaiaf@gmail.com>
2024-06-26 22:41:55 -0300

    feat(Frontend): :sparkles: resetTableSettings

--- src/pontosavi.ui/src/app/app/settings/page.tsx
+++ src/pontosavi.ui/src/app/app/settings/page.tsx
@@ -1,30 +1,33 @@
 "use client";
 
-import { Box, Button, Typography } from "@mui/material";
-import { ArrowBackIos } from "@mui/icons-material";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { InfoOutlined } from "@mui/icons-material";
+import { Box, Button, Tooltip, Typography } from "@mui/material";
+
+const resetTableSettings = () => {
+  const keys: string[] = [];
+
+  for (let i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i);
+    if (key) keys.push(key);
+  }
+
+  keys.filter(key => key.startsWith("@pontosavi:mrt-column-visibility-"))
+    .forEach(key => localStorage.removeItem(key));
+};
 
 export default function Page() {
-  const { back } = useRouter();
-
   return (
-    <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" gap={2}>
-      <Box display="flex" alignItems="center" width={{ xs: "80vw", md: "50vw", lg: "30vw" }}>
-        <Image src="https://c.tenor.com/ftaDAT-sWg4AAAAC/tenor.gif" alt="Imagem d" width={1080} height={1080} style={{ width: "100%", height: "auto" }} />
-      </Box>
+    <>
+      <Typography variant="h5" mb={2}>Settings</Typography>
 
-      <Box textAlign="center">
-        <Typography variant="h1">501</Typography>
-        <Typography variant="body1">Página não implementada :/</Typography>
-      </Box>
-
-      <Button variant="contained" startIcon={<ArrowBackIos />} onClick={() => back()}>
-        Voltar para a página anterior
-      </Button>
-      <Button href="/app">
-        Ir para a página inicial
-      </Button>
-    </Box>
+      <Box display="flex" alignItems="center" gap={2}>
+        <Button variant="contained" onClick={resetTableSettings}>
+          Reset table settings
+        </Button>
+        <Tooltip title="Reset show/hide columns. This will reset all columns visibility settings for all tables in the application." placement="right">
+          <InfoOutlined />
+        </Tooltip >
+      </Box >
+    </>
   );
 }
